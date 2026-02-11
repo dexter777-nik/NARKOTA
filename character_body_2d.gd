97685ce7ac9de5c7e0aaada1f9b1165449537099
dexter777-nik <alihanbabaev08@gmail.com>
2026-02-11 16:14:53 +0500

    клаассс классс класс
    пенис

---
+++ character_body_2d.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+# Скорость движения
+const SPEED = 300.0
+
+func _physics_process(delta):
+	# Получаем направление ввода (WASD или стрелки)
+	var direction = Input.get_vector("ui_left", "ui_right", "KEY_W", "ui_down")
+	
+	# Применяем скорость
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+
+	# Функция для обработки движения и столкновений
+	move_and_slide()
