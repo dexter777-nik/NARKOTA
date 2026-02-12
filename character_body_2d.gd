f3f9d8e65af344609537e966d6b642a901e933c4
Manazavrrrr <super.play340@gmail.com>
2026-02-12 13:15:58 +0500

    132312
    321321

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,17 +1,39 @@
 extends CharacterBody2D
 
-# Скорость движения
-const SPEED = 300.0
+const SPEED = 200.0
+@onready var _animated_sprite = $AnimatedSprite2D
 
-func _physics_process(delta):
-	# Получаем направление ввода (WASD или стрелки)
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+func _physics_process(_delta):
+	# Получаем вектор направления: (x: -1 до 1, y: -1 до 1)
+	var direction = Input.get_vector("left", "right", "up", "down")
 	
-	# Применяем скорость
-	if direction:
+	# Управляем физикой (движение)
+	if direction != Vector2.ZERO:
 		velocity = direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 
-	# Функция для обработки движения и столкновений
 	move_and_slide()
+	
+	# Управляем анимацией
+	update_animation(direction)
+
+func update_animation(direction):
+	if direction == Vector2.ZERO:
+		# Если стоим на месте - останавливаем анимацию
+		_animated_sprite.stop() 
+		# Или включи анимацию покоя, если есть: _animated_sprite.play("idle")
+	else:
+		# Выбираем анимацию в зависимости от того, куда жмет игрок
+		if abs(direction.x) > abs(direction.y):
+			# Если движение по горизонтали сильнее
+			if direction.x > 0:
+				_animated_sprite.play("walk_right")
+			else:
+				_animated_sprite.play("walk_left")
+		else:
+			# Если движение по вертикали сильнее
+			if direction.y > 0:
+				_animated_sprite.play("walk_down")
+			else:
+				_animated_sprite.play("walk_up")
