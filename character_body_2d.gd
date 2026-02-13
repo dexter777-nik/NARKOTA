7bf1eeeb796dfecdb0663f77b98ec6e0115c107c
Manazavrrrr <super.play340@gmail.com>
2026-02-13 11:09:59 +0500

    cjccb
    sosi

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -29,3 +29,12 @@ func _physics_process(delta):
 		$AnimatedSprite2D.stop()
 
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://park.tscn")
+
+
+func _on_club_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	get_tree().change_scene_to_file("res://клуб.tscn")
